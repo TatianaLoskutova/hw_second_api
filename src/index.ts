481835be879be407092346b563044fa0f55c2305
TatianaLoskutova <tanya.loskutova333@gmail.com>
2023-06-04 14:53:41 +0300

    All v 1.2

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import {TestingRouter} from './routers/testing_router';
 
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 // create middleware
 const parserMiddleware = bodyParser({})
