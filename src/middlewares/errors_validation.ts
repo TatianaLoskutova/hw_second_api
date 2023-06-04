5b5f294b97402e6b540c985c6a9e27abdb1e0709
TatianaLoskutova <tanya.loskutova333@gmail.com>
2023-06-04 19:10:59 +0300

    All v 2.4

--- src/middlewares/errors_validation.ts
+++ src/middlewares/errors_validation.ts
@@ -5,11 +5,11 @@ import {ErrorsType} from '../types';
 export const errorsMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        // const errorsMessages = errors.array().map((error) => ({
-        //     message: error.msg,
-        //     field: error.
-        // }));
-        res.status(400).send({ errors: errors.array() });
+        const errorsMessages = errors.array().map((error) => ({
+            message: error.msg,
+            path: error.msg
+        }));
+        // res.status(400).send({ errorsMessages: errors.array() });
     } else {
         next()
     }
