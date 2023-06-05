61336077f89c3f08bba8a7d5b6104b029c8240bb
TatianaLoskutova <tanya.loskutova333@gmail.com>
2023-06-05 11:42:22 +0300

    All v 3.2

--- src/repositories/blog_repository.ts
+++ src/repositories/blog_repository.ts
@@ -9,7 +9,7 @@ export const blogRepository = {
         let foundBlog = blogsDataBase.find(p => p.id === id)
         return foundBlog;
     },
-    createBlog(id: string,name: string, description: string, websiteUrl: string) {
+    createBlog(id:string, name: string, description: string, websiteUrl: string) {
         const newBlog = {
             id: Math.random().toString(36),
             name: name,
