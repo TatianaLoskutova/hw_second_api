bd94db21d962fbc5674e73a39074d827e1e76e1e
TatianaLoskutova <tanya.loskutova333@gmail.com>
2023-06-05 13:41:50 +0300

    All v 3.5

--- src/middlewares/blog_validator.ts
+++ src/middlewares/blog_validator.ts
@@ -1,11 +1,11 @@
 import {body} from 'express-validator';
 
-export const blogNameValidation = body('name').exists().bail()
-        .trim().isLength({min: 1, max: 15}).bail()
+export const blogNameValidation = body('name')
+        .trim().isLength({min: 1, max: 15}).withMessage('xxx')
+        .isString().withMessage('yyy')
+export const blogDescriptionValidation = body('description')
+        .trim().isLength({min: 1, max: 500})
         .isString()
-export const blogDescriptionValidation = body('description').exists().bail()
-        .trim().isLength({min: 1, max: 500}).bail()
-        .isString()
-export const blogWebsiteUrlValidation = body('"websiteUrl').exists().matches('^https://([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$')
+export const blogWebsiteUrlValidation = body('websiteUrl').matches('^https://([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$')
         .isLength({min: 1 , max: 100})
 
--- src/middlewares/errors_validation.ts
+++ src/middlewares/errors_validation.ts
@@ -1,15 +1,17 @@
 import {NextFunction, Request, Response} from 'express';
 import {ValidationError, validationResult} from 'express-validator';
+import {FieldValidationError} from 'express-validator/src/base';
 
 
 export const errorsMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-        res.status(400).send({errorsMessages: errors.array({onlyFirstError: true}).map(p => {
-            return {
-                message: p.msg,
-                field: p.type
-            }
+    const errors = validationResult(req).array({onlyFirstError: true})
+    console.log(errors)
+    if (errors.length > 0) {
+        res.status(400).send({errorsMessages: errors.map((p:FieldValidationError)=> {
+                return {
+                    message: p.msg,
+                    field: p.path
+                }
             })
         })
     } else {
